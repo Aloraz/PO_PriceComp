f7770cfe7e268ab5948f373db86aa39c92aecc4f
Aloraz <grzegorzaloraz@gmail.com>
2026-01-20 16:02:32 +0100

    zmiany kosmetyczne

--- obj/Debug/net8.0/projektPO.AssemblyInfo.cs
+++ obj/Debug/net8.0/projektPO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projektPO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4720d5c63247cfd3015bce9b4fd9f677d9213249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ed10525a0f8e399fe2b975778b12033cd885dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("projektPO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projektPO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
