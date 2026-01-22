804dd186fd310f0fc53ce1f6263cc4003f300a33
krystianmateja <krmateja@student.agh.edu.pl>
2026-01-22 14:31:06 +0100

    Add files via upload

---
+++ Test1.cs
@@ -0,0 +1,72 @@
+﻿using projektPO;
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Test_OnlineStore_ObliczanieCenyCalkowitej()
+        {
+            // Arrange
+            var sklep = new OnlineStore("Allegro", 15.00m);
+            var produkt = new Product("Kawa", 0.5m, "kg");
+
+            // Act
+            var oferta = new Offer(produkt, sklep, 40.00m);
+
+            // Assert
+            Assert.AreEqual(55.00m, oferta.TotalPrice);
+        }
+
+
+        [TestMethod]
+        public void Test_Product_CenaJednostkowaPowinnaBycPoprawna()
+        {
+            // Arrange
+            var produkt = new Product("Cukier", 2.0m, "kg");
+            var sklep = new LocalStore("Lidl");
+
+            // Act
+            var oferta = new Offer(produkt, sklep, 10.00m);
+
+            // Assert
+            Assert.AreEqual(5.00m, oferta.UnitPrice);
+        }
+        [TestMethod]
+        public void Test_Offer_WyjatekPrzyUjemnej()
+        {
+            // Arrange
+            var sklep = new LocalStore("Biedronka");
+            var produkt = new Product("Chleb");
+
+            // Act & Assert
+            Assert.ThrowsException<InvalidPriceException>(() =>
+                new Offer(produkt, sklep, -5.00m)
+            );
+        }
+        [TestMethod]
+        public void Test_Product_Equals()
+        {
+            // Arrange
+            var p1 = new Product("Mleko", 1.0m, "l");
+            var p2 = new Product("mleko", 1.0m, "l"); // mała litera, ale ta sama nazwa
+
+            // Act
+            bool saRowne = p1.Equals(p2);
+
+            // Assert
+            Assert.IsTrue(saRowne, "Błąd: IEquatable nie rozpoznał produktów mimo tej samej nazwy i wagi.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
